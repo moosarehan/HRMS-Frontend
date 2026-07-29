3fa70facdb7905bca4355dfbd072c19aa709347a
moosarehan <moosarehan4@gmail.com>
2026-07-29 12:58:17 +0500

    feat: wire up AuthProvider and LeaveProvider in main entry point

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { LeaveProvider } from './context/LeaveContext.jsx'
+import './styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <LeaveProvider>
+        <App />
+      </LeaveProvider>
+    </AuthProvider>
+  </React.StrictMode>,
+)
+
